1462fbce938fdeb3991c424001b59f938fca1ff9
Longthuat1 <longthuat2409@gmail.com>
2023-11-30 23:29:02 +0700

    Update1130

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-document.getElementById('bar').onclick = function(){
+document.getElementById('bar').onclick = function () {
     document.getElementById('bar').style.display = 'none';
     document.getElementById('times').style.display = 'block';
 }
+
+$(document).ready(function () {
+    $(".popup-with-zoom-anim").magnificPopup({
+        type: "inline",
+
+        fixedContentPos: false,
+        fixedBgPos: true,
+
+        overflowY: "auto",
+
+        closeBtnInside: true,
+        preloader: false,
+
+        midClick: true,
+        removalDelay: 300,
+        mainClass: "my-mfp-zoom-in"
+    });
+});
